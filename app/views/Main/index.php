5ba835f6eeb2da8c569c7d506e232bf4412464a7
Edin Jašarević <edinjasar14@gmail.com>
2018-11-01 12:44:32 +0100

    glavni pogled

---
+++ app/views/Main/index.php
@@ -0,0 +1,16 @@
+<?php include 'app/views/_global/beforeContent.php'; ?>
+
+<article class="row">
+    <div class="col-xs-12">
+        <h1>Početna stranica</h1>
+        <div class="page-content">
+<pre>
+Prvi koraci su da se promeni Configuration.php i da se setuje prava putanja sajta i podaci za bazu podataka.
+Zatim treba napraviti adekvatne modele za tabele baze podataka, kontrolere i view generatore za njihove metode, kao i adekvatne rute.
+U svakom koraku testirati rad aplikacije.
+</pre>
+        </div>
+    </div>
+</article>
+
+<?php include 'app/views/_global/afterContent.php'; ?>
