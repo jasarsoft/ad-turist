509d383a483411681f0de27078f64ba5c2e5ecae
Edin Jašarević <edinjasar14@gmail.com>
2018-11-08 02:03:42 +0100

    tag id iz venue

--- app/models/TagModel.php
+++ app/models/TagModel.php
@@ -31,4 +31,28 @@
                 return null;
             }
         }
+        
+        /**
+         * Metod vraca niz objekata koji sadrze podatke o smjestejnim
+         * kapacitetima koji su obiljezeni tagom ciji ID broj je dat kao
+         * argument metoda
+         * @param int $id ID broj taga
+         * @return array
+         */
+        public static function getVenuesForTagId($id) {
+            $id = intval($id);
+            $SQL = 'SELECT * FROM venue_tag WHERE tag_id = ?;';
+            $prep = DataBase::getInstance()->prepare($SQL);
+            $res = $prep->execute([$id]);
+            if ($res) {
+                $spisak = $prep->fetchAll(PDO::FETCH_OBJ);
+                $list = [];
+                foreach ($spisak as $item) {
+                    $list[] = VenueModel::getById($item->venue_id);
+                }
+                return $list;
+            } else {
+                return null;
+            }
+        }
     }
