0a801d790456d187facc3f2a37f1887228e04dee
Edin Jašarević <edinjasar14@gmail.com>
2018-11-27 23:05:09 +0100

    lista kategorija za gosta

---
+++ app/views/Main/listByCategory.php
@@ -0,0 +1,13 @@
+<?php include 'app/views/_global/beforeContent.php'; ?>
+
+<article class="row">
+    <div class="col-xs-12">
+        <div class="page-content">
+<pre>
+Lista smjestajnih kapaciteta
+</pre>
+        </div>
+    </div>
+</article>
+
+<?php include 'app/views/_global/afterContent.php'; ?>
