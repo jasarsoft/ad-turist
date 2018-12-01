5523747d7a85f36b2cda80e519a057dfdf3ce47c
Edin Jašarević <edinjasar14@gmail.com>
2018-12-01 23:00:37 +0100

    lista slika

---
+++ app/views/AdminVenueImage/listVenueImages.php
@@ -0,0 +1,36 @@
+<?php include 'app/views/_global/beforeContent.php'; ?>
+
+<article class="row">
+    <div class="col-xs-12">
+        <h1>Spisak svih slika</h1>
+        <div class="page-content">
+            <table class='table table-hover table-condensed'>
+                <thead>
+                    <tr>
+                        <td colspan='4' class='align-right'>
+                            <?php Misc::url('admin/images/venue/' . $DATA['venue_id'] . '/add' , 'Add new image to this venue'); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Id</th>
+                        <th>Image</th>
+                        <th>Options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($DATA['images'] as $image): ?>
+                    <tr>
+                        <td><?php echo $image->image_id; ?></td>
+                        <td>
+                            <img src='<?php echo Configuration::BASE_PATH . htmlspecialchars($image->path); ?>' class='small-image' >
+                        </td>
+                        <td><?php Misc::url('admin/images/venue/' . $DATA['venue_id'] . '/edit/' . $image ->image_id, 'Delete'); ?></td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</article>
+
+<?php include 'app/views/_global/afterContent.php'; ?>
