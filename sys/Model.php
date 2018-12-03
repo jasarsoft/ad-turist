9fd98d1b600f23ee141bddd5cf816830ef5de6cb
Edin Jašarević <edinjasar14@gmail.com>
2018-12-03 21:51:07 +0100

    edit metod modela

--- sys/Model.php
+++ sys/Model.php
@@ -3,7 +3,7 @@
 /**
  * Osnovna klasa svih modela
  */
-abstract class Model /*implements ModelInterface*/ {
+abstract class Model implements ModelInterface {
     /**
      * Metod vraca ime tabele na osnovu imena klase modela
      * @return string
@@ -48,6 +48,80 @@ abstract class Model /*implements ModelInterface*/ {
             return $prep->fetch(PDO::FETCH_OBJ);
         } else {
             return null;
+        }
+    }
+    
+     /**
+     * Metod koji daje stavku sa podacima datim u nizu u adekvatnu tabelu u bazi;
+     * @param array $data 
+     * @return int|boolean Vrijednost primarnog kljuca za novi zapis u tabeli ili FALSE ako doslo do greske
+     */
+    public static function add(array $data) {
+        $tableName = self::getTableName();
+        $nizImenaPolja = [];
+        $nizPlaceholdera = [];
+        $nizVrijednosti = [];
+        
+        foreach ($data as $imePolja => $vrijednostPolja) {
+            if (preg_match('/^[a-z][a-z0-9\_]*$/', $imePolja) and $imePolja != $tableName . '_id') {
+                if (is_object($vrijednostPolja) or is_array($vrijednostPolja)) {
+                    continue;
+                }
+                
+                $nizImenaPolja = $imePolja;
+                $nizPlaceholdera = '?';
+                $nizVrijednosti = $vrijednostPolja;
+            }
+        }
+        
+        $spisakPolja = implode(', ', $nizImenaPolja);
+        $spisakUpitnika = implode(', ', $nizPlaceholdera);
+        
+        $SQL = 'INSERT INTO ' . $tableName . ' (' . $spisakPolja . ') VALUES (' . $spisakUpitnika . ');';
+        $prep = DataBase::getInstance()->prepare($SQL);
+        if ($prep) {
+            $res = $prep->execute($nizVrijednosti);
+            if ($res) {
+                return DataBase::getInstance()->lastInsertId();
+            } else {
+                return FALSE;
+            }
+        } else {
+            return FALSE;
+        }
+    }
+    
+     /**
+     * Ovaj metod mjenja podatke iz data niza na zapisu tabele pod ID broje $id
+     * @param int $id 
+     * @param array $data 
+     * @return boolean Vraca true ako je uspjesan upit, a false ako nije;
+     */
+    public static function edit(int $id, array $data) {
+        $tableName = self::getTableName();
+        $nizPromjena = [];
+        $nizVrijednosti = [];
+        
+        foreach ($data as $imePolja => $vrijednostPolja) {
+            if (preg_match('/^[a-z][a-z0-9\_]*$/', $imePolja) and $imePolja != $tableName . '_id') {
+                if (is_object($vrijednostPolja) or is_array($vrijednostPolja)) {
+                    continue;
+                }
+                
+                $nizPromjena = $imePolja . ' = ?';
+                $nizVrijednosti = $vrijednostPolja;
+            }
+        }
+        
+        $spisakPromjena = implode(', ', $nizPromjena);
+        
+        $SQL = 'UPDATE ' . $tableName . ' SET ' . $spisakPromjena . ' WHERE ' . $tableName . '_id = ?;';
+        $prep = DataBase::getInstance()->prepare($SQL);
+        if ($prep) {
+            $nizVrijednosti[] = $id;
+            return $prep->execute($nizVrijednosti);
+        } else {
+            return FALSE;
         }
     }
 }
